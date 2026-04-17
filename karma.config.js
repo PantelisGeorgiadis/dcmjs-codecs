4ecfc2a5ea7ace2cd5a9ee85d02c3efe06964304
Pantelis Georgiadis <PantelisGeorgiadis@Gmail.com>
2026-04-17 11:59:04 +0300

    Updated karma configuration

--- karma.config.js
+++ karma.config.js
@@ -1,25 +1,35 @@
 module.exports = (config) => {
   config.set({
-    frameworks: ['browserify', 'mocha', 'chai', 'sinon'],
+    autoWatch: false,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    browsers: ['ChromeHeadless'],
+    colors: true,
+    concurrency: Infinity,
     files: [
       'test/**/*.test.js',
       {
-        pattern: 'wasm/bin/dcmjs-native-codecs.wasm',
         included: false,
-        watched: false,
+        pattern: 'wasm/bin/dcmjs-native-codecs.wasm',
         served: true,
+        watched: false,
       },
     ],
+    flags: [
+      '--no-sandbox',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--remote-debugging-port=9222',
+    ],
+    frameworks: ['browserify', 'mocha', 'chai', 'sinon'],
+    logLevel: config.LOG_INFO,
+    port: 9876,
     preprocessors: {
       'test/**/*.test.js': 'browserify',
     },
     reporters: ['mocha'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
-    autoWatch: false,
     singleRun: true,
-    concurrency: Infinity,
   });
 };
